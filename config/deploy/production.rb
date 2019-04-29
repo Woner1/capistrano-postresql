376849b646dcc0fba4aec57192e9fe2406590a86
aaa <aaa@iMac-2.local>
2019-04-29 09:59:03 +0800

    commit

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,12 +3,11 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "192.168.31.48", user: "ankh", roles: %w{app db web}, my_property: :my_value
+server "192.168.31.47", user: "ankh", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 set :nginx_server_name, '192.168.31.47'
 
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
 
 
 # role-based syntax
@@ -42,11 +41,11 @@ set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
 # http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
 #
 # Global options
-# set :ssh_options, {
-#     keys: %w(~/.ssh/id_rsa),
-#     forward_agent: true,
-#     auth_methods: %w(publickey)
-# }
+set :ssh_options, {
+    keys: %w(~/.ssh/id_rsa),
+    forward_agent: true,
+    auth_methods: %w(publickey)
+}
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
